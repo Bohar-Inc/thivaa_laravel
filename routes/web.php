ed22605a8b9200af63e4a677d7f806c06ac65cc7
ThivaaVipul <thivaavipul@gmail.com~>
2025-04-17 17:08:34 +0530

    livewire-customizationl

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,19 @@
 <?php
 
+use App\Livewire\Admin\AdminDashboard;
 use App\Livewire\Settings\Appearance;
 use App\Livewire\Settings\Password;
 use App\Livewire\Settings\Profile;
 use Illuminate\Support\Facades\Route;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
 Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+    ->middleware(['auth', 'verified', 'teacher'])
+    ->name('teacher.dashboard');
 
 Route::middleware(['auth'])->group(function () {
     Route::redirect('settings', 'settings/profile');
@@ -19,6 +21,10 @@ Route::middleware(['auth'])->group(function () {
     Route::get('settings/profile', Profile::class)->name('settings.profile');
     Route::get('settings/password', Password::class)->name('settings.password');
     Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
+});
+
+Route::middleware(['admin', 'auth'])->group(function () {
+    Route::get('/admin/dashboard', AdminDashboard::class)->name('admin.dashboard');
 });
 
 require __DIR__.'/auth.php';
