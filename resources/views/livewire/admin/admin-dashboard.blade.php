f2db9e97e2eaf3cb972abade053eb3ee42b29765
ThivaaVipul <thivaavipul14@gmail.com>
2025-04-18 17:31:25 +0530

    Dashboard & Analytics

--- resources/views/livewire/admin/admin-dashboard.blade.php
+++ resources/views/livewire/admin/admin-dashboard.blade.php
@@ -1,3 +1,5 @@
 <div>
-    {{-- A good traveler has no fixed plans and is not intent upon arriving. --}}
+    <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
+        <livewire:dashboard-widget-overview/>
+    </div>
 </div>
